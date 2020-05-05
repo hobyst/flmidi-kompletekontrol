1e5e875db7fc0452515218f34e35b55e0b98df3e
hobyst <48522412+hobyst@users.noreply.github.com>
2020-05-05 17:17:09 +0200

    Removed nihia as a module

--- nihia/extended.py
+++
@@ -1,5 +0,0 @@
-# Extended functionality library for using the NIHIA protocol
-
-# This script contains all the functions and methods needed to take advantage of the deep integration
-# features that are exclusive of the more advanced NI devices like the S-Series keyboards or the Maschine MK3
-# like the real-time peak meters on the mixer view, the colors on the pads (for Maschine devices) and so on.
