68fb37876acad2074c9087ef3dc718046bfd2bef
hobyst <48522412+hobyst@users.noreply.github.com>
2021-11-07 21:37:48 +0100

    Docs: Fix font issue with latest versions of Furo

--- docs/conf.py
+++ docs/conf.py
@@ -52,6 +52,16 @@ html_css_files = {
     "text_elements.css"
 }
 
+html_theme_options = {
+    "light_css_variables": {
+        "font-stack": "Oswald, sans-serif"
+    },
+
+    "dark_css_variables": {
+        "font-stack": "Oswald, sans-serif"
+    }
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
